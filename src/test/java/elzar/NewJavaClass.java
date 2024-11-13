59662deee8f7232be42f5d832efc70f165f831b2
elzar <elzosman4@gmail.com>
2024-11-13 11:21:24 -0600

    elzars part

---
+++ src/test/java/elzar/NewJavaClass.java
@@ -0,0 +1,8 @@
+package elzar;
+
+public class NewJavaClass {
+    public static void main(String[] args) {
+        System.out.println("This is Elzar's part");
+        System.out.println("This is my project");
+    }
+}
