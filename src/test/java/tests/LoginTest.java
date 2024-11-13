61fdf3a20851ea2c71db17c4a8f76fd728130ca5
zafar2826138 <2826138@gmail.com>
2024-11-13 11:47:04 -0500

    deleted spaces

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -12,20 +12,18 @@ public class LoginTest {
 
     @Test
     public void testSuccess() {
+
         WebDriver driver = new ChromeDriver();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-
         LoginPage loginPage = new LoginPage(); // Fixed typo from loginPagen to loginPage
         loginPage.login("Admin", "admin123");
         Assert.assertTrue(driver.getCurrentUrl().contains("dashboard"));
         System.out.print("hi");
     }
-
     @Test
     public void testUnSuccess() {
         WebDriver driver = new ChromeDriver();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-
         LoginPage loginPage = new LoginPage(); // Fixed typo from loginPagen to loginPage
         loginPage.login("Admin", "adm123");
         Assert.assertTrue(loginPage.invalidLog.isDisplayed(), "Invalid login message not displayed");
