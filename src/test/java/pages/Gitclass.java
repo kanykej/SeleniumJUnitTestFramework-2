0956df6a756bcad0ed3deda7546328514d5b8637
kanykeiboron <k.boron93@gmail.com>
2024-11-12 22:26:40 -0600

    new code, new branch

--- src/test/java/pages/Gitclass.java
+++ src/test/java/pages/Gitclass.java
@@ -5,5 +5,7 @@ import java.sql.SQLOutput;
 public class Gitclass {
     public static void main(String[] args) {
         System.out.println("hello , it's for github");
+        System.out.println("I am from Kanykei's branch");
+        System.out.println("today november 12th");
     }
 }
