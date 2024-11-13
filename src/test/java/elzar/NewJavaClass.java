994a072e27dd017b6500a5b35503886c7083ebd4
elzar <elzosman4@gmail.com>
2024-11-13 12:04:17 -0600

    changes

--- src/test/java/elzar/NewJavaClass.java
+++ src/test/java/elzar/NewJavaClass.java
@@ -2,6 +2,19 @@ package elzar;
 public class NewJavaClass {
     public static void main(String[] args) {
         System.out.println("This is Elzar's part");
-        System.out.println("This is my project");
+        System.out.println("I made a change");
+        System.out.println("I made a change");
+        System.out.println("I made a change");
+        System.out.println("I made a change");
+        System.out.println("I made a change");
+        System.out.println("I made a change");
+        System.out.println("I made a change");
+        System.out.println("I made a change");
+        System.out.println("I made a change");
+        System.out.println("I made a change");
+        System.out.println("I made a change");
+        System.out.println("I made a change");
+        System.out.println("I made a change");
+        System.out.println("I made a change");
     }
 }
